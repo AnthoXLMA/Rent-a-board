095f7e38825eec548544fa770aeb3492cc2c2c1f
AnthoXLMA <contact@service-lite-plus.com>
2021-08-18 09:46:40 +0200

    deployment #2

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -3,6 +3,7 @@ class BoardsController < ApplicationController
   def index
     @user = current_user
     @boards = Board.all
+    @user.avatar = @user
   end
 
   def new
@@ -13,8 +14,9 @@ class BoardsController < ApplicationController
   def create
     @user = current_user
     @board = Board.new(board_params)
+    @board.user = @user
     @board.save
-      redirect_to boards_path(@boards)
+    redirect_to boards_path(@boards)
   end
 
   def show
@@ -28,6 +30,7 @@ class BoardsController < ApplicationController
   end
 
   def board_params
-    params.require(:board).permit(:name, :size, :brand, :price, :contact, :description, :supplier_id, :address, :user_id, :picture)
+    params.require(:board).permit(:name, :size, :brand, :price, :contact,
+    :description, :supplier_id, :address, :user_id, :picture)
   end
 end
